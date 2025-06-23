b90bc714024588eea6f077d3c2bb5dc58669f22a
mdfeinstein <110838263+mdfeinstein@users.noreply.github.com>
2025-06-23 15:31:39 -0400

    added a formatter

--- simulator/interactions/InteractionTemplated.h
+++ simulator/interactions/InteractionTemplated.h
@@ -1,76 +1,80 @@
 #pragma once
 #include "Mover.h"
 #include <any>
-#include <vector>
-#include <tuple>
 #include <functional>
-#include <utility>
-#include <string>
 #include <memory>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
-
-template<typename GlobalParamTuple, typename MoverParamTuple>
+template <typename GlobalParamTuple, typename MoverParamTuple>
 struct InteractionTemplated;
 
 template <typename... GlobalParams, typename... MoverParams>
-struct InteractionTemplated<std::tuple<GlobalParams...>, std::tuple<MoverParams...>> {
-    std::tuple<GlobalParams...> globalParams;
-    std::function<void(Mover&, Mover&, std::tuple<GlobalParams...>, std::tuple<MoverParams...>, std::tuple<MoverParams...>)> applyFunction;
-    std::string name;
+struct InteractionTemplated<std::tuple<GlobalParams...>,
+                            std::tuple<MoverParams...>> {
+  std::tuple<GlobalParams...> globalParams;
+  std::function<void(Mover &, Mover &, std::tuple<GlobalParams...>,
+                     std::tuple<MoverParams...>, std::tuple<MoverParams...>)>
+      applyFunction;
+  std::string name;
 
-    InteractionTemplated(
-        std::tuple<GlobalParams...> globalParams,
-        std::function<void(Mover&, Mover&, std::tuple<GlobalParams...>, std::tuple<MoverParams...>, std::tuple<MoverParams...>)> applyFunction,
-        std::string name)
-        : globalParams(globalParams), applyFunction(applyFunction), name(name) {}
+  InteractionTemplated(
+      std::tuple<GlobalParams...> globalParams,
+      std::function<void(Mover &, Mover &, std::tuple<GlobalParams...>,
+                         std::tuple<MoverParams...>,
+                         std::tuple<MoverParams...>)>
+          applyFunction,
+      std::string name)
+      : globalParams(globalParams), applyFunction(applyFunction), name(name) {}
 
+  std::tuple<MoverParams...> getMoverParams(Mover &mover) {
+    return std::any_cast<std::tuple<MoverParams...>>(
+        mover.effectParams[this->name]);
+  }
 
-    std::tuple<MoverParams...> getMoverParams(Mover& mover) {
-        return std::any_cast<std::tuple<MoverParams...>>(mover.effectParams[this->name]);
-    }
-
-    void apply(Mover& mover1, Mover& mover2) {
-        applyFunction(mover1, mover2, globalParams, getMoverParams(mover1), getMoverParams(mover2));
-    }
+  void apply(Mover &mover1, Mover &mover2) {
+    applyFunction(mover1, mover2, globalParams, getMoverParams(mover1),
+                  getMoverParams(mover2));
+  }
 };
 
 struct InteractionWrapper {
-    // Type-erased wrapper for InteractionTemplated. Should implement apply.
-    template <typename GlobalParamTuple, typename MoverParamTuple>
-    InteractionWrapper(GlobalParamTuple globalParams,
-        std::function<void(Mover&, Mover&, GlobalParamTuple, MoverParamTuple, MoverParamTuple)> applyFunction,
-        std::string name)
-        : conceptPtr(
-            std::make_unique<Model<GlobalParamTuple, MoverParamTuple>>(
-                InteractionTemplated<GlobalParamTuple, MoverParamTuple>(globalParams, applyFunction, name)
-            )) {}
+  // Type-erased wrapper for InteractionTemplated. Should implement apply.
+  template <typename GlobalParamTuple, typename MoverParamTuple>
+  InteractionWrapper(GlobalParamTuple globalParams,
+                     std::function<void(Mover &, Mover &, GlobalParamTuple,
+                                        MoverParamTuple, MoverParamTuple)>
+                         applyFunction,
+                     std::string name)
+      : conceptPtr(std::make_unique<Model<GlobalParamTuple, MoverParamTuple>>(
+            InteractionTemplated<GlobalParamTuple, MoverParamTuple>(
+                globalParams, applyFunction, name))) {}
 
-    void apply(Mover& mover1, Mover& mover2) {
-        conceptPtr->apply(mover1, mover2);
-    }
+  void apply(Mover &mover1, Mover &mover2) {
+    conceptPtr->apply(mover1, mover2);
+  }
 
-    std::string getName() {
-        return conceptPtr->getName();
-    }
+  std::string getName() { return conceptPtr->getName(); }
 
 private:
-    struct Concept {
-        virtual ~Concept() = default;
-        virtual void apply(Mover& mover1, Mover& mover2) = 0;
-        virtual std::string getName() = 0;
-    };
+  struct Concept {
+    virtual ~Concept() = default;
+    virtual void apply(Mover &mover1, Mover &mover2) = 0;
+    virtual std::string getName() = 0;
+  };
 
-    template <typename GlobalParamTuple, typename MoverParamTuple>
-    struct Model : Concept {
-        InteractionTemplated<GlobalParamTuple, MoverParamTuple> interaction;
-        Model(InteractionTemplated<GlobalParamTuple, MoverParamTuple> interaction) : interaction(interaction) {}
-        void apply(Mover& mover1, Mover& mover2) override {
-            interaction.apply(mover1, mover2);
-        }
-        std::string getName() override {
-            return interaction.name;
-        }
-    };
+  template <typename GlobalParamTuple, typename MoverParamTuple>
+  struct Model : Concept {
+    InteractionTemplated<GlobalParamTuple, MoverParamTuple> interaction;
+    Model(InteractionTemplated<GlobalParamTuple, MoverParamTuple> interaction)
+        : interaction(interaction) {}
+    void apply(Mover &mover1, Mover &mover2) override {
+      interaction.apply(mover1, mover2);
+    }
+    std::string getName() override { return interaction.name; }
+  };
 
-    std::unique_ptr<Concept> conceptPtr;
+  std::unique_ptr<Concept> conceptPtr;
 };
